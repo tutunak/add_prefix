4e33509fc8b02ce132d15bdf39af9a3ade781843
tutunak <dk@tutunak.com>
2020-02-23 15:57:24 +0300

    PEP8 + add documentation

--- add_prefix.py
+++ add_prefix.py
@@ -15,11 +15,19 @@ def add_prefix(file_name: str, prefix: str, dest: str = None) -> None:
                 destination.write(f"{prefix}{line}")
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_name", type=str)
-    parser.add_argument("prefix", type=str)
-    parser.add_argument("--dest", type=str, required=False, help="Destination for out file, default is out_+file_name")
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        "Add prefix", description="Add prefix for ech string in file"
+    )
+    var = parser.add_help
+    parser.add_argument("file_name", type=str, help="Source file")
+    parser.add_argument("prefix", type=str, help="Strings prefix")
+    parser.add_argument(
+        "--dest",
+        type=str,
+        required=False,
+        help="Destination for out file, default is out_+file_name"
+    )
     args = parser.parse_args()
     add_prefix(file_name=args.file_name, prefix=args.prefix, dest=args.dest)
 
